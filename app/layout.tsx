e3164b64c067718ae3ce15a44941d3e659f37d74
IgorTheFixer <75816411+IgorTheFixer@users.noreply.github.com>
2023-06-13 19:31:59 -0400

    adding additional packages

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,5 @@
+import Navbar from '@/components/Navbar'
 import './globals.css'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Cedric Jean-Philippe',
@@ -16,6 +14,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="dark:bg-stone-900">
+        <Navbar />
         {children}
       </body>
     </html>
