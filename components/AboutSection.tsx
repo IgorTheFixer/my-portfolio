a8005bd43c9dd0e602cb54444a67770d0bce020f
IgorTheFixer <75816411+IgorTheFixer@users.noreply.github.com>
2023-06-14 20:57:38 -0400

    added about page

---
+++ components/AboutSection.tsx
@@ -0,0 +1,92 @@
+import Image from "next/image"
+
+const skills = [
+  { skill: "HTML" },
+  { skill: "CSS" },
+  { skill: "JavaScript" },
+  { skill: "TypeScript" },
+  { skill: "Python" },
+  { skill: "React" },
+  { skill: "Next.js" },
+  { skill: "Tailwind CSS" },
+  { skill: "Git" },
+  { skill: "GitHub" },
+  { skill: "Jupyter Notebooks" },
+]
+
+const AboutSection = () => {
+  return (
+    <section id="about">
+      <div className="my-12 pb-12 md:pt-16 md:pb-48">
+        <h1 className="text-center font-bold text-4xl">
+          About Me
+          <hr className="w-6 h-1 mx-auto my-4 bg-teal-500 border-0 rounded"></hr>
+        </h1>
+
+        <div className="flex flex-col space-y-10 items-stretch justify-center align-top md:space-x-10 md:space-y-0 md:p-4 md:flex-row md:text-left">
+          <div className="md:w-1/2 ">
+            <h1 className="text-center text-2xl font-bold mb-6 md:text-left">
+              Get to know me!
+            </h1>
+            <p>
+              Hi, my name is Cedric and I am a{" "}
+              <span className="font-bold">{"highly ambitious"}</span>,
+              <span className="font-bold">{" self-motivated"}</span>, and
+              <span className="font-bold">{" driven"}</span> software engineer
+              based in Boston, MA.
+            </p>
+            <br />
+            <p>
+              I graduated from Brandeis University in 2017
+              with a BS in Biology in preparation to attend medical school.
+              However, I realized that it wasn't truly my dream.
+            </p>
+            <br />
+            <p>
+              After working and exploring various other fields, I fell in love with
+              software engineering and decided to dedicate my heart to making a name
+              for myself in the world of tech. I quickly attended a bootcamp and found 
+              a job not long after.
+            </p>
+            <br />
+            <p>
+              I believe that you should{" "}
+              <span className="font-bold text-teal-500">
+                never stop growing
+              </span>{" "}
+              and that&#39;s what I strive to do, I have a passion for
+              technology and a desire to always push the limits of what is
+              possible. I am excited to see where my career takes me and am
+              always open to new opportunities. 🙂
+            </p>
+          </div>
+          <div className="text-center md:w-1/2 md:text-left">
+            <h1 className="text-2xl font-bold mb-6">My Skills</h1>
+            <div className="flex flex-wrap flex-row justify-center z-10 md:justify-start">
+              {skills.map((item, idx) => {
+                return (
+                  <p
+                    key={idx}
+                    className="bg-gray-200 px-4 py-2 mr-2 mt-2 text-gray-500 rounded font-semibold"
+                  >
+                    {item.skill}
+                  </p>
+                )
+              })}
+            </div>
+            {/* find potential cartoon avatar */}
+            {/* <Image
+              src="/cartoon-image.png"
+              alt=""
+              width={325}
+              height={325}
+              className="hidden md:block md:relative md:bottom-4 md:left-32 md:z-0"
+            /> */}
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default AboutSection
